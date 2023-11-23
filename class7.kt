891088ff0d4944d656f7e0cbf74c701628445fac
dipuAppDev <dipuappdev@gmail.com>
2023-11-23 12:48:38 +0600

    update file

---
+++ class7.kt
@@ -0,0 +1,44 @@
+
+
+fun main(){
+    /**
+     * if-else practice
+     */
+
+    /*    var a:Int = 10
+        var b:Double = a.toDouble()
+        println(
+            b
+        )
+        var str:String = "22"
+        var integer:Int = str.toInt()
+        println(integer)
+        var ch : Char = 'F'
+        var integer2 : Int = ch.toInt()
+        println(integer2)*/
+
+    /**
+     * While loop
+     */
+
+//    var num : Int = 9990
+//    while(num<=10000){
+//        println(num)
+//        num++
+//    }
+    /**
+     * print names using while loop and array
+     */
+    /*  var names = arrayOf("Dipu","Sakib","Rakib","Akib","Hasan")
+      var index = 0
+      while(index<names.size){
+          println(names[index])
+          index++
+      }*/
+    /**
+     *
+     */
+
+
+
+}
